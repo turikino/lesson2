2cfe93322dd0763a3b6c09d393e684840ecb6d9d
turikino <turikino84@gmail.com>
2019-02-26 12:26:59 +0300

    Доделал задание с циклами

--- lesson2_for.py
+++ lesson2_for.py
@@ -3,11 +3,15 @@ import random
 school_classes = ['1a', '1b', '1c', '2a', '2b', '2c']
 school_all_classes_score = []
 for school_class in school_classes:
-    scores = [random.randrange(1, 5, 1) for score in range(5)]
+    scores = [random.randrange(1, 6, 1) for score in range(5)]
     school_class_dict = {'school_class' : school_class, 'scores' : scores}
     school_all_classes_score.append(school_class_dict)
 
+all_class_score = []
 for school_class in school_all_classes_score:
-    sum_score = sum(school_class["scores"])
-    print("В классе {} срединий балл: {}.".format(school_class["school_class"], sum_score))
+    class_sum_score = sum(school_class["scores"])/len(school_class["scores"])
+    all_class_score.append(class_sum_score)
+    print("В классе {} срединий балл: {}.".format(school_class["school_class"], int(class_sum_score)))
 
+all_classes_sum_score = sum(all_class_score)/len(school_classes)
+print("Во всей школе средний балл: {}.".format(int(all_classes_sum_score)))
